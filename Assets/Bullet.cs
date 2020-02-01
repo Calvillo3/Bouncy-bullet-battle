3f784b7ed38512b2b6514702afc85f301985dfac
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2020-01-31 21:04:20 -0600

    Bullets!
    Adding bullets that bounce around.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float bounds;
+
+    Collider2D coll;
+    Rigidbody2D rb;
+
+    GameObject shooter;
+    bool escapedPlayer;
+    Vector2 direction;
+    int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        escapedPlayer = false;
+        damage = 1;
+
+        rb.transform.rotation = Quaternion.AngleAxis(rb.rotation, Vector3.forward);
+        direction = rb.transform.rotation * Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is so we aren't wasting memory handling bullets off screen
+        if (Mathf.Abs(rb.position.x) > bounds || Mathf.Abs(rb.position.x) > bounds)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+    public void Claim(GameObject player)
+    {
+        shooter = player;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            // This is too avoid hitting ourselves once we fire it
+            // Technically if that happens the bullet should still be a trigger,
+            // But this is an extra precaution. 
+            if (collision.gameObject != shooter || escapedPlayer)
+            {
+                collision.gameObject.GetComponent<PlayerMovement>().TakeDamage(damage);
+                Destroy(this.gameObject);
+            }
+        }
+        // We will need to add a case for hitting a zombie.
+        // Though that should look similar to hitting a player.
+        else
+        {
+            Bounce();
+        }
+    }
+
+    void OnTriggerExit2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.gameObject == shooter && !escapedPlayer)
+        {
+            escapedPlayer = true;
+            coll.isTrigger = false;
+        }
+    }
+    //This is for the case where a player stupidly stands in front of a wall and shoots
+    void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.gameObject != shooter)
+        {
+            if (!escapedPlayer)
+            {
+                shooter.GetComponent<PlayerMovement>().TakeDamage(damage);
+            }
+        }
+    }
+
+    // Note that bouncing is easier since bullets are spherical
+    // If we made them anything else, we'd have to worry about 
+    // how they are oriented when the bounce.
+    void Bounce()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, direction, 1f, LayerMask.GetMask("Wall"));
+        Vector2 normal = hit.normal;
+        direction = direction - 2 * Vector2.Dot(direction, normal) * normal;
+        rb.MovePosition(rb.position + direction * 10 * speed * Time.fixedDeltaTime);
+    }
+}
+
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float turnSpeed;
+    [SerializeField] Bullet bullet;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
 
+    float turn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,15 +26,27 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        turn = Input.GetAxis("HorizontalJoyR");
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // Shoots a bullet in the direction the player is facing
+            Bullet pewpew = Instantiate(bullet,rb.position,rb.transform.rotation);
+            // We need to tell the bullet we shot it so it doesn't hurt the player... at first
+            pewpew.Claim(this.gameObject);
+        }
     }
 
     void FixedUpdate()
+    { 
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.SetRotation(rb.rotation - turn * turnSpeed * Time.fixedDeltaTime);
+    }
+
+    public void TakeDamage(int amount)
     {
-        Vector3 targetMovePos = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
-        RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, movement, moveSpeed * Time.fixedDeltaTime, LayerMask.GetMask("Wall"));
-        if (raycastHit.collider == null)
-        {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        }
+        Debug.Log("Ow! at " + Time.time + " seconds.");
+        //TODO Give the player health which ticks down after getting hit.
     }
 }
