3b35e87699688c2455661bbb60a5c88de07d2f65
Geogeo357 <geogeo357@gmail.com>
2020-02-04 00:28:35 -0600

    2 player controls
    co-op implemented

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,7 +9,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float turnSpeed;
     [SerializeField] Bullet bullet;
 
+    [SerializeField] int playerNum;
+
     public Rigidbody2D rb;
+    bool readyToShoot = true;
 
     Vector2 movement;
 
@@ -24,13 +27,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("HorizontalPlayer" + playerNum);
+        movement.y = Input.GetAxisRaw("VerticalPlayer" + playerNum) * -1;
 
-        turn = Input.GetAxis("HorizontalJoyR");
+        turn = Input.GetAxis("HorizontalJoyRPlayer" + playerNum);
 
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetAxisRaw("Fire1Player" + playerNum) < 0.5)
         {
+            readyToShoot = true;
+        }
+
+        if (Input.GetAxisRaw("Fire1Player" + playerNum) >= 0.99 && readyToShoot)
+        {
+            readyToShoot = false;
             // Shoots a bullet in the direction the player is facing
             Bullet pewpew = Instantiate(bullet,rb.position,rb.transform.rotation);
             // We need to tell the bullet we shot it so it doesn't hurt the player... at first
