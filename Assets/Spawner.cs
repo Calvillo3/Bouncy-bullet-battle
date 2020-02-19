88ea20b55793d6b5c9b83c3053e31808ea9004e4
Geogeo357 <geogeo357@gmail.com>
2020-02-19 17:20:44 -0600

    update waves
    easier start now

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -16,10 +16,11 @@ public class Spawner : MonoBehaviour
     GameObject currspawner;
     int[][] waveEnemies = new int[][]
     {
+        new int[] { 0 },
         new int[] { 0, 0, 0, 0, 0 },
-        new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        new int[] { 1, 1, 1, 1, 1 },
-        new int[] { 0, 0, 0, 0, 1, 1, 1, 1, 1 },
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+        new int[] { 1, 1, 2, 2 },
+        new int[] { 1, 2, 3, 3, 0, 0, 0 },
         new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
         new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         new int[] { 0, 0, 0, 0, 2, 2, 2, 2, 2 },
