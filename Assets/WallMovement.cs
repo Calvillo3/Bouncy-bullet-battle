d3dc82754f9eeba0610b263bde8c321f5cea13c2
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2020-02-19 02:12:01 -0600

    Walls Spin!

---
+++ Assets/WallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    Transform transform;
+    [SerializeField] float RPM;
+    float incrementAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        incrementAngle = RPM / 60 * 360 * Time.fixedDeltaTime; 
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, incrementAngle);
+    }
+}
