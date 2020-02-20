525f56e231b0476bee9c240e18e25428cce355b0
Caleb Calvillo <calvillocaleb3@gmail.com>
2020-02-20 02:37:58 -0600

    Main Menu stuff and something interesting

---
+++ Assets/BulletMM.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMM : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float bounds;
+    [SerializeField] GameObject explosion;
+    Vector2 direction;
+    Rigidbody2D rb;
+    Gradient grad;
+    CircleCollider2D coll;
+    
+
+    int bounceCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounceCount = 0;
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CircleCollider2D>();
+        direction = rb.transform.rotation * Vector2.up;
+
+        grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(GetComponent<SpriteRenderer>().color, 0.0f), explosion.GetComponent<ParticleSystem>().colorOverLifetime.color.gradient.colorKeys[1] }, explosion.GetComponent<ParticleSystem>().colorOverLifetime.color.gradient.alphaKeys);
+        GetComponent<TrailRenderer>().colorGradient = grad;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Mathf.Abs(rb.position.x) > bounds || Mathf.Abs(rb.position.x) > bounds)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Bounce();
+    }
+
+    void Bounce()
+    {
+        if (bounceCount >= 4) {
+            Destroy(gameObject);
+        }
+        else {
+           
+            bounceCount++;
+            RaycastHit2D hit = Physics2D.CircleCast(rb.position, coll.radius, direction, speed * Time.fixedDeltaTime, LayerMask.GetMask("Wall"));
+            Vector2 normal = hit.normal;
+            direction = direction - 2 * Vector2.Dot(direction, normal) * normal;
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        }
+        var col = explosion.GetComponent<ParticleSystem>().colorOverLifetime;
+        col.color = grad;
+        Instantiate(explosion, transform.position, transform.rotation).SetActive(true);
+
+    }
+}
---
+++ Assets/MainMenuBounce.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBounce : MonoBehaviour
+{
+
+    Color32[] colorArray = {new Color32(0 ,228, 255, 255), new Color32(0 ,255, 40, 255), new Color32(255, 78, 0, 255)};
+    [SerializeField] float timeBulletSpawn;
+    [SerializeField] Tethers tetherlist;
+    Transform[] tethertracker;
+    [SerializeField] BulletMM prefab;
+    float timer;
+    int randomtether;
+    int randomcolor;
+    int randomangle;
+    Vector3 angle;
+    void Start()
+    {
+        tethertracker = tetherlist.GetComponentsInChildren<Transform>();
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= timeBulletSpawn) {
+            //do stuff
+            randomtether = Random.Range(0, colorArray.Length);
+            randomcolor = Random.Range(0, colorArray.Length);
+            randomangle = Random.Range(0, 359);
+            
+            angle = new Vector3(0, 0, randomangle);
+            BulletMM pewpew = Instantiate(prefab, tethertracker[randomcolor].position, Quaternion.Euler(angle));
+            pewpew.GetComponent<SpriteRenderer>().color = colorArray[randomcolor];
+            timer = 0;
+        }
+        else {
+            timer += Time.deltaTime;
+        }
+    }
+}
