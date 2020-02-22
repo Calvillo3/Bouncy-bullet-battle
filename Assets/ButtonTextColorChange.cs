6938491ba083fd559374242c64fb0c389bf55619
Caleb Calvillo <calvillocaleb3@gmail.com>
2020-02-22 05:03:39 -0600

    Menu

---
+++ Assets/ButtonTextColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+public class ButtonTextColorChange : MonoBehaviour, ISelectHandler, IDeselectHandler {
+
+    [SerializeField] Color color;
+    [SerializeField] Button button;
+
+    Color grey = new Color(105, 195, 195, 255);
+ 
+     public void OnSelect(BaseEventData eventData)
+     {
+        
+         button.GetComponentInChildren<TextMeshProUGUI>().color = color;
+ 
+     }
+     public void OnDeselect(BaseEventData eventData)
+         {
+             button.GetComponentInChildren<TextMeshProUGUI>().color = grey;
+     
+         }
+     }
