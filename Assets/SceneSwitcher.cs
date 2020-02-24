5fdc656b6e82974c185f9b6d352b6e5cfd276b83
Caleb Calvillo <calvillocaleb3@gmail.com>
2020-02-24 15:56:23 -0600

    pushed final menu

--- Assets/SceneSwitcher.cs
+++ Assets/SceneSwitcher.cs
@@ -14,6 +14,8 @@ public class SceneSwitcher : MonoBehaviour
     [SerializeField] Button button4;
     [SerializeField] Button button5;
     [SerializeField] Button button6;
+    [SerializeField] Button button7;
+    [SerializeField] Button button8;
 
     // Start is called before the first frame update
     void Start()
@@ -21,25 +23,50 @@ public class SceneSwitcher : MonoBehaviour
         button1.onClick.AddListener(RunGame);
         button2.onClick.AddListener(RunOptions);
         button3.onClick.AddListener(QuitGame);
-        button4.onClick.AddListener(PlayChallenge);
+        button4.onClick.AddListener(PlayCoop);
+        button6.onClick.AddListener(PlayEasy);
+        button7.onClick.AddListener(PlayMedium);
+        button8.onClick.AddListener(PlayHard);
     }
     void Update() {
         if(Input.GetButtonDown("BackButton")) {
+            if(button4.gameObject.activeSelf) {
             button1.gameObject.SetActive(true);
             button2.gameObject.SetActive(true);
             button3.gameObject.SetActive(true);
             button4.gameObject.SetActive(false);
             button5.gameObject.SetActive(false);
-            button6.gameObject.SetActive(false);
             button4.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
             button5.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
-            button6.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
             button1.Select();
             button1.OnSelect(null);
+            }
+            if(button6.gameObject.activeSelf) {
+                button4.gameObject.SetActive(true);
+                button5.gameObject.SetActive(true);
+                button6.gameObject.SetActive(false);
+                button7.gameObject.SetActive(false);
+                button8.gameObject.SetActive(false);
+                button6.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+                button7.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+                button8.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+                button4.Select();
+                button4.OnSelect(null);
+
+            }
         }
     }
 
     // Update is called once per frame
+    void PlayEasy() {
+        SceneManager.LoadScene("Easy");
+    }
+    void PlayMedium() {
+        SceneManager.LoadScene("Medium");
+    }
+    void PlayHard() {
+        SceneManager.LoadScene("Hard");
+    }
     void RunGame() {
         //
        button1.gameObject.SetActive(false);
@@ -47,14 +74,19 @@ public class SceneSwitcher : MonoBehaviour
        button3.gameObject.SetActive(false);
        button4.gameObject.SetActive(true);
        button5.gameObject.SetActive(true);
-       button6.gameObject.SetActive(true);
        button4.Select();
        button4.OnSelect(null);
 
     
     }   
-    void PlayChallenge() {
-        SceneManager.LoadScene("GameScene");
+    void PlayCoop() {
+       button4.gameObject.SetActive(false);
+       button5.gameObject.SetActive(false);
+       button6.gameObject.SetActive(true);
+       button7.gameObject.SetActive(true);
+       button8.gameObject.SetActive(true);
+       button6.Select();
+       button6.OnSelect(null);
     }
     void RunOptions() {
         //
