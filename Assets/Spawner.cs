daf0ae62853f57f442cc065be8905bdbdcd6cda0
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2020-02-28 20:00:10 -0600

    Non-Hard Coded Enemies
    The ability to include hard coded enemies is not yet there

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -16,6 +16,57 @@ public class Spawner : MonoBehaviour
     Transform[] spawners;
     GameObject currspawner;
 
+    // These params can be played with at 
+    // https://docs.google.com/spreadsheets/d/1IfBrU4DdlSni5aqVUrOrI0jhVTFAdG4zZzFWZSZqzjI/edit#gid=0
+    // with graphs showing their effect
+    Dictionary<string, float>[] spawnParams = new Dictionary<string, float>[]
+    {
+        // x = level, y = wave
+        // Enemy 0
+        // 10log(x) + 2log(y) + -1.3x + 0y + 2
+        new Dictionary<string, float> 
+        {
+            {"log(level)", 10f},
+            {"log(wave)", 2f},
+            {"level", -1.3f},
+            {"wave", 0f},
+            {"constant", 2f} 
+        },
+        // Enemy 1
+        // 1log(x) + 1log(y) + 0.1x + 0.1y + 0
+        new Dictionary<string, float> 
+        {
+            {"log(level)", 1f},
+            {"log(wave)", 1f},
+            {"level", .1f},
+            {"wave", .1f},
+            {"constant", 0f} 
+        },
+        // Enemy 2 
+        // -0.2log(x) + 1log(y) + 0.3x + 0.1y + -1
+        new Dictionary<string, float> 
+        {
+            {"log(level)", -.2f},
+            {"log(wave)", 1f},
+            {"level", 0.3f},
+            {"wave", 0.1f},
+            {"constant", -1} 
+        },
+        // Enemy 3
+        // 0log(x) + 0log(y) + 0.5x + 0.5y + -5
+        new Dictionary<string, float> 
+        {
+            {"log(level)", 0f},
+            {"log(wave)", 0f},
+            {"level", 0.5f},
+            {"wave", 0.5f},
+            {"constant", -5} 
+        },
+    };
+
+
+
+
     int[][] waveEnemies = new int[][]
     {
         new int[] { }
@@ -63,8 +114,8 @@ public class Spawner : MonoBehaviour
 
     void Start()
     {
-        int[][][] waveOptions = new int[][][] { waveEnemies0, waveEnemies1, waveEnemies2 };
-        waveEnemies = waveOptions[waveSelector];
+        //int[][][] waveOptions = new int[][][] { waveEnemies0, waveEnemies1, waveEnemies2 };
+        //waveEnemies = waveOptions[waveSelector];
         nextSpawnTime = 0;
         spawners = gameObject.GetComponentsInChildren<Transform>();
         currtime = 2;
@@ -86,15 +137,58 @@ public class Spawner : MonoBehaviour
             int waveNumber = int.Parse(waveText.text.Substring(6));
             waveText.text = "Wave: " + (waveNumber + 1).ToString();
             currtime = 0;
-            // if they've reached the last wave, just repeat it till they lose lol
-            waveNumber = Mathf.Min(waveNumber, waveEnemies.Length - 1);
-            for (int i = 0; i < waveEnemies[waveNumber].Length; i++) {
-                EnemyMovement newFoe = Instantiate(enemies[waveEnemies[waveNumber][i]], spawners[i%spawners.Length].position, Quaternion.identity);
+
+            // Previously waveEnemies referred to an array comprised of waves comprised of enemies
+            // Now this only refers to the enemies for a single wave
+            int[] waveEnemies = GenerateWave(waveSelector, waveNumber); //waveSelector is like level for now
+
+            for (int i = 0; i < waveEnemies.Length; i++) {
+                EnemyMovement newFoe = Instantiate(enemies[waveEnemies[i]], spawners[i%spawners.Length].position, Quaternion.identity);
                 newFoe.gameObject.SetActive(true);
             } 
         }
         if (GameObject.FindGameObjectWithTag("Enemy") == null) {
             currtime += Time.deltaTime;
         }
+    }
+
+    // Generate at array of enemies, using level and wave as input
+    int[] GenerateWave(int level, int wave)
+    {
+        List<int> waveEnemies = new List<int>();
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            // Use the parameter to decide how to weight level and wave
+            int numOfEnemy = Mathf.RoundToInt(
+                  spawnParams[i]["log(level)"] * Mathf.Log10(level)
+                + spawnParams[i]["log(wave)"] * Mathf.Log10(wave)
+                + spawnParams[i]["level"] * level
+                + spawnParams[i]["wave"] * wave
+                + spawnParams[i]["constant"]);
+
+            // Don't try to make negative numbers of enemies
+            numOfEnemy = Mathf.Max(numOfEnemy, 0);
+            
+            // reformats it to align with existing code
+            for (int j = 0; j < numOfEnemy; j++)
+            {
+                waveEnemies.Add(i);
+            }
+        }
+        
+        // If for some reason, no enemies are to be spawn, make one weak enemy
+        if (waveEnemies.Count == 0)
+        {
+            waveEnemies.Add(0);
+        }
+        string result = "List contents: ";
+        foreach (var item in waveEnemies)
+        {
+            result += item.ToString() + ", ";
+        }
+        Debug.Log(result);
+
+        // We've been playing with this as a list to expand it, but elsewhere we need an array
+        return waveEnemies.ToArray();
     }
 }
