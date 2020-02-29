69aae83ce164101a5ce2e253fc53e433da8363ef
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2020-02-28 19:59:35 -0600

    Stop Looping Sound
    Sound plays in main menu and continues without being destroyed

---
+++ Assets/DontDestroySoundOnLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroySoundOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        AudioSource[] sounds = FindObjectsOfType<AudioSource>();
+
+        if (sounds.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
