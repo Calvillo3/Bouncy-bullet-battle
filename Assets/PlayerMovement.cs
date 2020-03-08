925cb11be506f9692f275ffcc65e593526750145
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2020-03-07 21:18:31 -0600

    Invisible Bug Fix
    If the player comes back mid blink they sometimes still have renderer not fully enabled. This should fix that.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -125,6 +125,7 @@ public class PlayerMovement : ShooterMovement
             if (Time.time > invincibleUntilTime)
             {
                 invincible = false;
+                ren.enabled = true;
             }
             else
             {
